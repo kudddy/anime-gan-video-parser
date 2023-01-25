0fd97eba7b18531f29c928a0a104e4965751828e
Худяков Кирилл Константинович <Khudyakov.K.Ko@sberbank.ru>
2023-01-25 23:18:38 +0300

    delete async client

--- plugins/workers/crop_photo/crop_photo.py
+++ plugins/workers/crop_photo/crop_photo.py
@@ -43,13 +43,19 @@ def insert_face_from_photo(file_id: str):
 
     image_to_np = cv2.cvtColor(image_to_np, cv2.COLOR_BGR2RGB)
 
-    detected_faces = face_detector(image_to_np, 1)
+    max_height = image_to_np.shape[0]
+    max_width = image_to_np.shape[1]
 
+    detected_faces = face_detector(image_to_np, 1)
     if len(detected_faces) > 0:
         face_rect = detected_faces[0]
 
-        crop = image_to_np[face_rect.top() - x_top:face_rect.bottom() + x_bottom,
-               face_rect.left() - x_left:face_rect.right() + x_right]
+        top = 0 if face_rect.top() - x_top < 0 else face_rect.top() - x_top
+        bottom = max_height if face_rect.bottom() + x_bottom > max_height else face_rect.bottom() + x_bottom
+        left = 0 if face_rect.left() - x_left < 0 else face_rect.left() - x_left
+        right = max_width if face_rect.right() + x_right > max_width else face_rect.right() + x_right
+
+        crop = image_to_np[top:bottom, left:right]
         crop = cv2.imencode('.jpg ', crop, [cv2.IMWRITE_JPEG_QUALITY, 100])[1].tobytes()
     else:
         crop = cv2.imencode('.jpg ', image_to_np, [cv2.IMWRITE_JPEG_QUALITY, 100])[1].tobytes()
